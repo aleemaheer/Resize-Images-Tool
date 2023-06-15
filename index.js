ff3b5a5799dee87c7dda696c52b247c2a21efdbb
aleem aheer <aleemaheer5769644@gmail.com>
2023-06-15 18:25:28 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const sharp = require("sharp");
+
+const folderPath = "images";
+
+// Read the files in the folder
+fs.readdir(folderPath, (err, files) => {
+	if (err) {
+		console.error("Error reading folder:", err);
+		return;
+	}
+
+	// Process each file
+	files.forEach((file) => {
+		const filePath = `${folderPath}/${file}`;
+
+		// Check if the file is a JPG image
+		if (file.endsWith(".jpeg")) {
+			// Resize the image using sharp
+			sharp(filePath)
+				.resize(820, 820)
+				.toBuffer((resizeErr, buffer) => {
+					if (resizeErr) {
+						console.error(`Error resizing image ${file}:`, resizeErr);
+						return;
+					}
+
+					// Replace the original file with the resized image
+					fs.writeFile(filePath, buffer, (writeErr) => {
+						if (writeErr) {
+							console.error(`Error replacing image ${file}:`, writeErr);
+							return;
+						}
+
+						console.log(`Image ${file} resized and replaced successfully.`);
+					});
+				});
+		}
+	});
+});
